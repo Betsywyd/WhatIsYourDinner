2a487c3ecaf04f24f0b8fba89739618b71ba8edd
Tim Holleque <timothy.t.holleque@gmail.com>
2023-06-22 16:58:39 -0400

    Rapid API connected/recipes working

--- WhatIsForDinnerBackEnd/WhatIsForDinnerBackEnd/Models/spoonacularDAL.cs
+++ WhatIsForDinnerBackEnd/WhatIsForDinnerBackEnd/Models/spoonacularDAL.cs
@@ -7,9 +7,12 @@ namespace WhatIsForDinnerBackEnd.Models
         public Recipe GetRecipe(int id)
         {
             
-            RestClient rc = new RestClient($"https://api.spoonacular.com/recipes/{id}/information?includeNutrition=false");
+            RestClient rc = new RestClient($"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/{id}/information?includeNutrition=false");
             RestRequest rr = new RestRequest();
-            rr.AddQueryParameter("apiKey", Secret.ApiKey);
+            rr.AddHeader("X-RapidAPI-Key", Secret.ApiKey);
+            rr.AddHeader("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+
+
             Task<Recipe> recipe = rc.GetAsync<Recipe>(rr);
             Recipe r = recipe.Result;
 
@@ -20,25 +23,27 @@ namespace WhatIsForDinnerBackEnd.Models
         public RecipeSearchResult GetRecipeResult(string input)
         {
 
-            RestClient client = new RestClient($"https://api.spoonacular.com/recipes/complexSearch?query={input}");
+            RestClient client = new RestClient($"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query={input}");
             RestRequest request = new RestRequest();
-            request.AddQueryParameter("apiKey", Secret.ApiKey);
+            request.AddHeader("X-RapidAPI-Key", Secret.ApiKey);
+            request.AddHeader("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
             Task<RecipeSearchResult> response = client.GetAsync<RecipeSearchResult>(request);
   
             return response.Result;
         }
 
-        public Ingredient GetIngredient(int id)
-        {
+        //public Ingredient GetIngredient(int id)
+        //{
 
-            RestClient rc = new RestClient($"https://api.spoonacular.com/food/ingredients/{id}/information?amount=1");
-            RestRequest rr = new RestRequest();
-            rr.AddQueryParameter("apiKey", Secret.ApiKey);
-            Task<Ingredient> ingredient = rc.GetAsync<Ingredient>(rr);
-            Ingredient i = ingredient.Result;
+        //    RestClient rc = new RestClient($"https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/ingredients/{id}/information?amount=1");
+        //    RestRequest rr = new RestRequest();
+        //    rr.AddHeader("X-RapidAPI-Key", Secret.ApiKey);
+        //    rr.AddHeader("X-RapidAPI-Host", "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com");
+        //    Task<Ingredient> ingredient = rc.GetAsync<Ingredient>(rr);
+        //    Ingredient i = ingredient.Result;
 
-            return i;
-        }
+        //    return i;
+        //}
 
     }
 }
